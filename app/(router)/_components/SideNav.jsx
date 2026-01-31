752ee63866c80e91ea147b964b8908c3ca80fc75
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2026-01-31 18:50:34 +0800

    feat(page): add side banner on main page
    add get side banner GraphQL query into GlobalApi.js
    add component SideBanners.jsx into courses folder
    include side banner component into page.js
    hydrated error if grammarly extention is enabled

--- app/(router)/_components/SideNav.jsx
+++ app/(router)/_components/SideNav.jsx
@@ -51,8 +51,8 @@ function SideNav() {
       {/* Menu List */}
       <div className='mt-5'>
         {menu.map((item,index) => (
-          <Link href={item.path}>
-          <div key={item.id} className={`group flex gap-3 mt-2 p-3 text-[18px] items-center text-gray-500 cursor-pointer hover:bg-primary hover:text-white rounded-md transition-all ease-in-out duration-200
+          <Link key={item.id} href={item.path}>
+          <div className={`group flex gap-3 mt-2 p-3 text-[18px] items-center text-gray-500 cursor-pointer hover:bg-primary hover:text-white rounded-md transition-all ease-in-out duration-200
           ${path.includes(item.path)&&'bg-primary text-white'}
           `}>
             <item.icon className='group-hover:animate-bounce'/>
---
+++ app/(router)/courses/_components/SideBanners.jsx
@@ -0,0 +1,27 @@
+import GlobalApi from '@/app/_utils/GlobalApi'
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+
+function SideBanners() {
+  const [sideBannerList,setSideBannerList]=useState([]);
+  useEffect(()=>{
+    getSideBanners();
+  },[])
+  const getSideBanners=()=>{
+    GlobalApi.getSideBanner().then(resp=>{
+      console.log(resp);
+      setSideBannerList(resp.sideBanners)
+    })
+  }
+  return (
+    <div>
+      {sideBannerList.map((item,index)=>(
+        <div key={index}>
+          <Image src={item.banner.url} alt='banner' width={500} height={300} onClick={()=>window.open(item?.url)} className='rounded-xl cursor-pointer mb-4'/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SideBanners
